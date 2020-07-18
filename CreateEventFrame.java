87cd9fdf4d022a4aea8414a8beba012f93d55897
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-07-17 19:38:49 -0700

    Add CreateEventFrame

---
+++ CreateEventFrame.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CreateEventFrame extends JFrame{
+    public CreateEventFrame() {
+        JFrame frame = new JFrame("Create a New Event");
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new GridLayout(7,2));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,200);
+        frame.setVisible(true);
+
+
+        JLabel eventName = new JLabel("Event Name: ");
+        JTextField eventNameInput = new JTextField(15);
+
+        JLabel startMonth = new JLabel("Starting Month: ");
+        JTextField startMonthInput = new JTextField();
+
+        JLabel endMonth = new JLabel("Ending Month: ");
+        JTextField endMonthInput = new JTextField();
+
+        JLabel startTime = new JLabel("Starting Time: ");
+        JTextField startTimeInput= new JTextField();
+
+        JLabel endTime = new JLabel("Ending Time: ");
+        JTextField endTimeInput = new JTextField();
+
+        JButton saveBtn = new JButton("Save");
+        JButton cancelBtn = new JButton("Cancel");
+
+
+        panel.add(eventName);
+        panel.add(eventNameInput);
+        panel.add(startMonth);
+        panel.add(startMonthInput);
+        panel.add(endMonth);
+        panel.add(endMonthInput);
+        panel.add(startTime);
+        panel.add(startTimeInput);
+        panel.add(endTime);
+        panel.add(endTimeInput);
+        panel.add(saveBtn);
+        panel.add(cancelBtn);
+
+        frame.add(panel);
+
+        // Add actionListener
+        saveBtn.addActionListener(e -> {
+            System.out.println("Debug: I'm here -> saveBtn");
+            // parse input
+            // Check time conflict
+            // Save new event to Model
+
+        });
+
+        cancelBtn.addActionListener(e -> {
+            frame.dispose();
+        });
+    }
+
+}
